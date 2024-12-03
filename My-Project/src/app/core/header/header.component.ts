a605a79feddb50393bd38d583b8293d01d1afe3b
AleksandarV12 <aleksandyr.velkov12@gmail.com>
2024-12-03 19:47:03 +0100

    Header and footer updates

--- My-Project/src/app/core/header/header.component.ts
+++ My-Project/src/app/core/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  onLogin() {
+    console.log('Login button clicked');
+  }
 
+  onRegister() {
+    console.log('Register button clicked');
+  }
 }
